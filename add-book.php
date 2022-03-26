264d18ab3b7d2d839fda56eda503b12f84d8a536
Bhavana Katari <bhavana.katari09@gmail.com>
2022-03-26 08:36:52 -0500

    Initial Commit

---
+++ add-book.php
@@ -0,0 +1,133 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(strlen($_SESSION['alogin'])==0)
+    {   
+header('location:index.php');
+}
+else{ 
+
+
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>BOOK RESALE WITH LENDING AND DONATION | Add Book</title>
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONT AWESOME STYLE  -->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE  -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <!-- GOOGLE FONT -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+
+</head>
+<body>
+      <!------MENU SECTION START-->
+<?php include('includes/header.php');?>
+<!-- MENU SECTION END-->
+    <div class="content-wrapper">
+         <div class="container">
+        <div class="row pad-botm">
+            <div class="col-md-12">
+                <h4 class="header-line">Add Book</h4>
+                
+                            </div>
+
+</div>
+<div class="row">
+<div class="col-md-12 col-sm-12 col-xs-12">
+<div class="panel panel-info">
+<div class="panel-heading">
+Book Info
+</div>
+<div class="panel-body">
+<form role="form" method="post" enctype="multipart/form-data">
+
+<div class="col-md-6">   
+<div class="form-group">
+<label>Book Name<span style="color:red;">*</span></label>
+<input class="form-control" type="text" name="bookname" autocomplete="off"  required />
+</div>
+</div>
+
+<div class="col-md-6">   
+<div class="form-group">
+<label>Genre<span style="color:red;">*</span></label>
+<input class="form-control" type="text" name="genre" autocomplete="off"  required />
+</div>
+</div>
+
+<div class="col-md-6">   
+<div class="form-group">
+<label>Author<span style="color:red;">*</span></label>
+<input class="form-control" type="text" name="category" autocomplete="off"  required />
+</div>
+</div>
+
+<div class="col-md-6">   
+<div class="form-group">
+<label>Condition<span style="color:red;">*</span></label>
+<input class="form-control" type="text" name="condition" autocomplete="off"  required />
+</div>
+</div>
+
+
+
+<div class="col-md-6">  
+ <div class="form-group">
+ <label>Price<span style="color:red;">*</span></label>
+ <input class="form-control" type="text" name="price" autocomplete="off"   required="required" />
+ </div>
+</div>
+
+<div class="col-md-6">  
+ <div class="form-group">
+ <label>Type<span style="color:red;">*</span></label>
+ <input class="form-control" type="text" name="type" autocomplete="off"   required="required" />
+ </div>
+</div>
+
+<div class="col-md-6">  
+ <div class="form-group">
+ <label>Year of Publication<span style="color:red;">*</span></label>
+ <input class="form-control" type="text" name="yofp" autocomplete="off"   required="required" />
+ </div>
+</div>
+
+<div class="col-md-6">  
+ <div class="form-group">
+ <label>Book Picture<span style="color:red;">*</span></label>
+ <input class="form-control" type="file" name="bookpic" autocomplete="off"   required="required" />
+ </div>
+    </div>
+<button type="submit" name="add" id="add" class="btn btn-info">Submit </button>
+ </div>
+
+</div>
+ 
+                            </div>
+
+        </div>
+   
+    </div>
+    </div>
+     <!-- CONTENT-WRAPPER SECTION END-->
+  <?php include('includes/footer.php');?>
+      <!-- FOOTER SECTION END-->
+    <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+    <!-- CORE JQUERY  -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <!-- BOOTSTRAP SCRIPTS  -->
+    <script src="assets/js/bootstrap.js"></script>
+      <!-- CUSTOM SCRIPTS  -->
+    <script src="assets/js/custom.js"></script>
+</body>
+</html>
+<?php } ?>
